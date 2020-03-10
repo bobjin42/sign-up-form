f1e64a7b2af97d7f7cca3bd5879a83ad00b96ff7
Yicheng Jin <bobjin42@gmail.com>
2020-03-10 11:15:38 -0500

    react router initial setup

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import EmailPage from "../pages/EmailPage";
+import NamePage from "../pages/NamePage";
+import ConfirmationPage from "../pages/ConfirmationPage";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact render={() => <HomePage />} />
+          <Route path="/home" exact render={() => <HomePage />} />
+          <Route path="/email" exact render={() => <EmailPage />} />
+          <Route path="/name" exact render={() => <NamePage />} />
+          <Route
+            path="/confirmation"
+            exact
+            render={() => <ConfirmationPage />}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/NamePage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NamePage() {
+  return <div>Name</div>;
+}
